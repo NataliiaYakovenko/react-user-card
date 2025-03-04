56dbbb2e4806069ef60819d3395e8e4e2725613a
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-04 17:07:08 +0200

    Add gender check

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,6 +10,7 @@ function App() {
     firstName: "Amanda",
     lastName: "Taylor",
     position: "Model",
+    isFemale: true
   };
   return (
     <article className="cardUser">
@@ -17,35 +18,47 @@ function App() {
         src={userCard.imageUrl}
         userName={`${userCard.firstName} ${userCard.lastName}`}
         userPosition={userCard.position}
+        isFemale={userCard.isFemale}
       />
+      <UserButton />
+      <UserSocial />
     </article>
   );
 }
 
 function ComponentUser(props) {
-  const { src, userName, userPosition } = props;
+  const { src, userName, userPosition,isFemale } = props;
+ 
+  const userColor = isFemale ? 'wight': 'rgb(27, 27, 27)';
   return (
     <>
       <img src={src} alt="Model"></img>
-      <div className="cardUserName">{userName}</div>
+      <div className="cardUserName" style={{color:userColor}}>{userName}</div>
       <div className="cardUserPosition">{userPosition}</div>
-      <button className="cardUserButton">+</button>
-      <div className="social">
-        <div>
-          <span className="socialInfo">Views</span>
-          <span className="socialСalculation">1388</span>
-        </div>
-        <div>
-          <span className="socialInfo">Like</span>
-          <span className="socialСalculation">970</span>
-        </div>
-        <div>
-          <span className="socialInfo">Followers</span>
-          <span className="socialСalculation">554</span>
-        </div>
-      </div>
     </>
   );
 }
 
 
+function UserButton() {
+  return <button className="cardUserButton">+</button>;
+}
+
+function UserSocial() {
+  return (
+    <div className="social">
+      <div>
+        <span className="socialInfo">Views</span>
+        <span className="socialСalculation">1388</span>
+      </div>
+      <div>
+        <span className="socialInfo">Like</span>
+        <span className="socialСalculation">970</span>
+      </div>
+      <div>
+        <span className="socialInfo">Followers</span>
+        <span className="socialСalculation">554</span>
+      </div>
+    </div>
+  );
+}
